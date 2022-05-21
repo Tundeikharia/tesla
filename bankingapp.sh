3b74743a4775e6ccba879174c5a4970ba8b3729e
TUNDE IKHARIA <tundeikharia@yahoo.com>
2022-05-21 16:28:42 -0400

    first commit

---
+++ bankingapp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo welcome to TD bank
+echo
+echo pls enter your first and last  name
+read userName
+echo 
+echo welcome $userName
+echo
+echo please enter your password
+read password
+sleep 5s
+echo
+echo your transaction is successfull
+echo
+echo thank you for banking with us
+echo
+echo please $userName tell us how we can serve you better
+
